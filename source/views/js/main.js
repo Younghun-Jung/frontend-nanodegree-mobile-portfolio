690fad1d0b1e94709f10f898edf9a493e3489c46
Younghun-Jung <youngj6169@gmail.com>
2016-08-11 22:31:10 +0900

    change number of pizzas on screen in pizza.html

--- source/views/js/main.js
+++ source/views/js/main.js
@@ -538,7 +538,12 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+  var numCol = Math.ceil(window.innerWidth / s);
+  var numRow = Math.ceil(window.innerHeight / s);
+  var numPizzas = numCol * numRow;
+  var pizzas = document.getElementById('movingPizzas1');
+
+  for (var i = 0; i < numPizzas; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
@@ -546,7 +551,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
+    pizzas.appendChild(elem);
   }
   updatePositions();
 });
